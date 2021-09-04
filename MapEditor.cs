a877de981796e44ea04ffa46d500529f7d6375f0
Bacoq4 <53489761+Bacoq4@users.noreply.github.com>
2021-09-05 02:40:16 +0300

    Update MapEditor.cs

--- MapEditor.cs
+++ MapEditor.cs
@@ -7,10 +7,10 @@ using UnityEngine;
 [CustomEditor(typeof(MapGenerator))]
 public class MapEditor : Editor
 {
-    // obje seçili olduğu zaman çalışan bir metod
+    
     public override void OnInspectorGUI()
     {
-        // yardımcı fonksiyolar sağlanıyor base class tarafından
+        
         base.OnInspectorGUI();
 
         MapGenerator map = target as MapGenerator;
