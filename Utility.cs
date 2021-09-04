e72d10c4a32d8f0d0ba3f796d0d2df1376399ef4
Bacoq4 <53489761+Bacoq4@users.noreply.github.com>
2021-09-05 02:43:37 +0300

    Update Utility.cs

--- Utility.cs
+++ Utility.cs
@@ -4,17 +4,14 @@ using System.Collections.Generic;
 
 public static class Utility  
 {
-    // alınan dizinin elemanlarının karıştırılma metodu
     public static List<T> ShuffleList<T>(List<T> list , int seed){
 
         System.Random prng = new System.Random(seed);
 
         for (int i = 0; i < list.Count - 1; i++)
         {
-            // dizinin elemanları karıştırılıyor.
             int randomIndex = prng.Next(i,list.Count);
 
-            // basit bir şekilde elemanlar birbirinin yerine geçiyor.
             T tempItem = list[randomIndex];
             list[randomIndex] = list[i];
             list[i] = tempItem;
